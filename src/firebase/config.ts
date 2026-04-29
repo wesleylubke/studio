c9234a8bcacbec0962149e434608174759b031f9
Wesley Lübke <wclubke@gmail.com>
2026-04-29 01:47:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-139565229-26dcb",
+  "appId": "1:760671705829:web:7a68fd7bc0be812648920c",
+  "apiKey": "AIzaSyATs9oJ4htQCZCId6Ow9gj0ugaKj2ppnBo",
+  "authDomain": "studio-139565229-26dcb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "760671705829"
+};
